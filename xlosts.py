f937fac5a9dbd0b1807539748b0ff2c331e9270d
okzokzokz <62724093+okzokzokz@users.noreply.github.com>
2020-03-28 15:34:48 +0000

    Update xlosts.py

--- xlosts.py
+++ xlosts.py
@@ -8,8 +8,8 @@ def id_generator(size=5, chars=string.ascii_uppercase + string.ascii_lowercase):
     return ''.join(random.choice(chars) for _ in range(size))
 num = id_generator()
 
-r = ('https://soundcloud.com/jahseh-onfroy/prime-verse/s-'+num)
-response = requests.get('https://soundcloud.com/jahseh-onfroy/prime-verse/s-'+num)
+r = ('https://soundcloud.com/jahseh-onfroy/red-prod-yxxxnz-xxx/s-'+num)
+response = requests.get('https://soundcloud.com/jahseh-onfroy/red-prod-yxxxnz-xxx/s-'+num)
 m = response.status_code
 
 while(m!=a):
